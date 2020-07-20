3c17b88e25599cd02e23f653e5f5c13ab24f76eb
Josef Grieb <josef.grieb@gmail.com>
2020-07-20 14:24:59 +0200

    change sleep

--- transport-native-io_uring/src/main/java/io/netty/channel/uring/IOUringEventLoop.java
+++ transport-native-io_uring/src/main/java/io/netty/channel/uring/IOUringEventLoop.java
@@ -144,7 +144,7 @@ class IOUringEventLoop extends SingleThreadEventLoop {
                 runAllTasks();
             }
             try {
-                Thread.sleep(100);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
